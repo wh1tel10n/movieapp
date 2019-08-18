4e312a65959bc7811dd0fc34097194ab22304add
Simon Joo <sm.whitelion@gmail.com>
2019-08-19 06:22:32 +0900

    Updates

---
+++ precache-manifest.1ba4790b326054df3d8ac7c5a72da66d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "942facdc6092a2ee5403265142c340e1",
+    "url": "/movieapp/index.html"
+  },
+  {
+    "revision": "27b1e0d9351102fba208",
+    "url": "/movieapp/static/css/main.1a684b2e.chunk.css"
+  },
+  {
+    "revision": "7ae164a35ce8a771ffee",
+    "url": "/movieapp/static/js/2.88e6bcb5.chunk.js"
+  },
+  {
+    "revision": "27b1e0d9351102fba208",
+    "url": "/movieapp/static/js/main.3e19fe49.chunk.js"
+  },
+  {
+    "revision": "99104dda4a269b0969d5",
+    "url": "/movieapp/static/js/runtime~main.4703e8e8.js"
+  }
+]);
